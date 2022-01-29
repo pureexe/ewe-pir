a84aafe0aac7bc3d698eeabf40a0d30a4ae27422
Pakkapon Phongthawee <pure.gif@gmail.com>
2022-01-29 23:30:28 +0700

    add placeholder

---
+++ config.js
@@ -0,0 +1,6 @@
+// configuration file for easily changing
+// note that pre-save login-url is not follow Ewelink's API Guide
+// if you fork this repository and deploy on your own server. Please consider to implement it to dynamic version
+const EWEMOTION = {
+    'login_url': 'https://c2ccdn.coolkit.cc/oauth/index.html?state=dashboard&clientId=skppQBUJrSC3NIygYxu5XENCIjcxu7MV&authorization=Ym3s1fooI0EykgzkB78Zrvu+1pYPCwJ9qq/Q0eSRygk=&seq=1643473391153&redirectUrl=https://pureexe.github.io/ewemotion&grantType=authorization_code'
+}
